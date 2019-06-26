bd2f85e6b3c2abe16392ed333bf7519474a3345e
Harm Bruinsma <hbruinsma@cyclomedia.com>
2019-06-26 09:43:07 +0200

    STREET-2050: Fixed overlays with 2 open viewers

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Properties/AssemblyInfo.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CycloMedia")]
 [assembly: AssemblyProduct("Street Smart for ArcGIS Pro")]
-[assembly: AssemblyCopyright("Copyright © CycloMedia 2018 - 2019")]
+[assembly: AssemblyCopyright("Copyright © CycloMedia 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("914cc234-6eac-401d-a7a8-96baa1782909")]
 
-[assembly: AssemblyVersion("0.9.5.*")]
-[assembly: AssemblyFileVersion("0.9.5")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0")]
