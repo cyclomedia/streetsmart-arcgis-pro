5a631f932860c448bc67a19736aea8dfb44de844
Harm Bruinsma <hbruinsma@cyclomedia.com>
2023-11-30 15:08:30 +0100

    updated version number

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Properties/AssemblyInfo.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("914cc234-6eac-401d-a7a8-96baa1782909")]
 
-[assembly: AssemblyVersion("1.7.2")]
-[assembly: AssemblyFileVersion("1.7.2")]
+[assembly: AssemblyVersion("1.7.6")]
+[assembly: AssemblyFileVersion("1.7.6")]
