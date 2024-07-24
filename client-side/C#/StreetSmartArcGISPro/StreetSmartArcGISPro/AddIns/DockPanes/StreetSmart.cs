e04abada4de03d5dd1910bb448f49768f6f8b2e1
Aleksa Milivojevic <amilivojevic@cyclomedia.com>
2024-07-24 07:14:47 -0700

    Fix solution for crashing on 3.0

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
@@ -895,7 +895,7 @@ namespace StreetSmartArcGISPro.AddIns.DockPanes
             }
           }
 
-          IGeoJsonOverlay overlay = OverlayFactory.Create(geoJson, layerName, srsName, sld?.SLD, visible);
+          IGeoJsonOverlay overlay = OverlayFactory.Create(geoJson, layerName, srsName, sld?.GetSerializedSld(), visible);
           overlay = await Api.AddOverlay(overlay);
           StoredLayer layer = _storedLayerList.GetLayer(layerNameAndUri);
 
