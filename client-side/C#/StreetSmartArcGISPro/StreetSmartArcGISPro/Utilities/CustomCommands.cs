fd0573f5baf1140a7579146c66fa97f995f7d6fd
Aleksa Milivojevic <amilivojevic@cyclomedia.com>
2024-08-16 05:18:37 -0700

    Removed redundant KeyGesture definition from OAuthToBeVisibleCommand to simplify command setup.

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Utilities/CustomCommands.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Utilities/CustomCommands.cs
@@ -12,8 +12,7 @@ namespace StreetSmartArcGISPro.Utilities
     public static readonly RoutedUICommand OAuthToBeVisibleCommand = new RoutedUICommand(
         "OpenMethod",
         "OpenMethod",
-        typeof(CustomCommands),
-        new InputGestureCollection() { new KeyGesture(Key.O, ModifierKeys.Control) }
+        typeof(CustomCommands)
     );
   }
 }
