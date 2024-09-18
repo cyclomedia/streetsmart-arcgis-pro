7ae9d1b853cdf75756125d622c0cb5c9d40e19a8
AleksaMilivojevicCycloMedia <166837985+AleksaMilivojevicCycloMedia@users.noreply.github.com>
2024-09-18 11:37:37 +0200

    ESRI-170-Bug fixed when having 2 cyclorama and closing the first one both are closed (#113)
    ESRI-170 - Bug fixed - Changes related to fixing bug while closing the first cyclorama

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
@@ -1231,10 +1231,10 @@ namespace StreetSmartArcGISPro.AddIns.DockPanes
 
       if (Api != null && !_inRestart)
       {
-        IList<IViewer> viewers = await Api.GetViewers();
+        IList<IViewer> viewers = await Api.GetViewers(); //method returns wrong value
         int nrViewers = viewers.Count;
 
-        if (nrViewers == 0)
+        if (_viewerList.Count() == 0)
         {
           _inClose = false;
           DoHide();
