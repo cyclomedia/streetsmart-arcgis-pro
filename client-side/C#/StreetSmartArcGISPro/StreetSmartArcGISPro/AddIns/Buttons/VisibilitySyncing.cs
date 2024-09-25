01a05cc15c37cda444bbe3f309d61f637bb47ffb
Aleksa Milivojevic <amilivojevic@cyclomedia.com>
2024-09-25 02:47:58 -0700

    Changes related to syncing the visibility

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/Buttons/VisibilitySyncing.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/Buttons/VisibilitySyncing.cs
@@ -16,20 +16,20 @@ namespace StreetSmartArcGISPro.AddIns.Buttons
 {
   internal class VisibilitySyncing : Button
   {
-    private static readonly FileProjectList _projectList = FileProjectList.Instance;
-    private static FileSettings _settings = _projectList.GetSettings(MapView.Active);
-    private bool _checked = true;
+    private readonly FileProjectList _projectList;
+    private FileSettings _settings;
 
     protected VisibilitySyncing()
     {
-      _checked = DockPanestreetSmart.Current.ShouldSyncLayersVisibility();
-      IsChecked = _checked;
+      _projectList = FileProjectList.Instance;
+      _settings = _projectList.GetSettings(MapView.Active);
+
+      IsChecked = DockPanestreetSmart.Current.ShouldSyncLayersVisibility();
       ActiveMapViewChangedEvent.Subscribe(OnActiveMapViewChanged);
     }
     protected override async void OnClick()
     {
-      _checked = DockPanestreetSmart.Current.ShouldSyncLayersVisibility();
-      _settings.SyncLayerVisibility = !_checked;
+      _settings.SyncLayerVisibility = !(DockPanestreetSmart.Current.ShouldSyncLayersVisibility());
       _projectList.Save();
       IsChecked = !IsChecked;
       if (IsChecked)
@@ -38,8 +38,7 @@ namespace StreetSmartArcGISPro.AddIns.Buttons
 
     private void OnActiveMapViewChanged(ActiveMapViewChangedEventArgs args)
     {
-      _checked = DockPanestreetSmart.Current.ShouldSyncLayersVisibility();
-      IsChecked = _checked;
+      IsChecked = DockPanestreetSmart.Current.ShouldSyncLayersVisibility();
     }
   }
 }
