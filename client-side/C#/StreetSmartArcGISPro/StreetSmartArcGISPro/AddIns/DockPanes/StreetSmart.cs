58bee78a15f200c027fccad8124bb28f7443bc3a
Aleksa Milivojevic <amilivojevic@cyclomedia.com>
2024-10-02 01:01:59 -0700

    Changes related to task 244 for 2.9 version

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
@@ -401,7 +401,7 @@ namespace StreetSmartArcGISPro.AddIns.DockPanes
         {
           IList<IViewer> viewers = await Api.GetViewers();
 
-          if (viewers.Count >= 1)
+          if (viewers.Any())
           {
             try
             {
@@ -412,9 +412,11 @@ namespace StreetSmartArcGISPro.AddIns.DockPanes
               EventLog.Write(EventLog.EventType.Error, $"Street Smart: (StreetSmart.cs) (CloseViewersAsync): exception: {e}");
             }
           }
+          else
+          {
+            _inClose = false;
+          }
         }
-
-        _inClose = false;
       }
     }
 
