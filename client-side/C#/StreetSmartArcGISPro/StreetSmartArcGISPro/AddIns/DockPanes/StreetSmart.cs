39606cfc3f78b1866b2f01eee64c0d61d08e21f9
AleksaMilivojevicCycloMedia <166837985+AleksaMilivojevicCycloMedia@users.noreply.github.com>
2024-10-03 16:40:57 +0200

    ESRI-244 - Bug fixed (#128)
    ESRI-244 - Bug fixed - when closing the Map container having multiple cyclorama viewers open some of them still open

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/AddIns/DockPanes/StreetSmart.cs
@@ -361,7 +361,7 @@ namespace StreetSmartArcGISPro.AddIns.DockPanes
         {
           IList<IViewer> viewers = await Api.GetViewers();
 
-          if (viewers.Count >= 1)
+          if (viewers.Any())
           {
             try
             {
@@ -372,9 +372,11 @@ namespace StreetSmartArcGISPro.AddIns.DockPanes
               EventLog.Write(EventLog.EventType.Error, $"Street Smart: (StreetSmart.cs) (CloseViewersAsync): exception: {e}");
             }
           }
+          else
+          {
+            _inClose = false;
+          }
         }
-
-        _inClose = false;
       }
     }
 
