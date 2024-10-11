1cce12d4d9776342a2fec205d30e1091d4a59f38
Martin Sven Kovačić <mskovacic@cyclomedia.com>
2024-10-11 15:29:55 +0200

    fixed formatting

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/CycloMediaLayers/CycloMediaLayer.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/CycloMediaLayers/CycloMediaLayer.cs
@@ -41,7 +41,6 @@ using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using ArcGISProject = ArcGIS.Desktop.Core.Project;
 using MySpatialReference = StreetSmartArcGISPro.Configuration.Remote.SpatialReference.SpatialReference;
-using StreetSmartArcGISPro.Logging;
 
 
 
