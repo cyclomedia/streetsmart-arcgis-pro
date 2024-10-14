b7134b1b660348c3a74e2f8877a6ec5bddd899c2
Aleksa Milivojevic <amilivojevic@cyclomedia.com>
2024-10-14 15:58:40 +0200

    ESRI-235 - Proactive Insights - Spam protection

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Logging/EventLog.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Logging/EventLog.cs
@@ -2,9 +2,17 @@
 using System;
 using System.Runtime.CompilerServices;
 using static ArcGIS.Desktop.Framework.Utilities.EventLog;
+using FileConfiguration = StreetSmartArcGISPro.Configuration.File.Configuration;
 
 namespace StreetSmartArcGISPro.Logging
 {
+  public enum TimeUnit : short
+  {
+    Minute,
+    Hour,
+    Day
+  }
+
   public enum EventLogLevel : short
   {
     Debug,
@@ -15,10 +23,16 @@ namespace StreetSmartArcGISPro.Logging
 
   public static class EventLog
   {
+    private static int LogsLimit = LogData.Instance.LogLimit;
+    private static TimeUnit timeUnit = LogData.Instance.TimeUnit;
+    private static int logCount = 0;
+    private static DateTime lastReset;
+    private static LogData _logData = LogData.Instance;
     public static IDisposable InitializeSentry(string sentryDsnUrl)
     {
       try
       {
+        lastReset = DateTime.Now;
         return SentrySdk.Init(options =>
         {
           options.Dsn = sentryDsnUrl;
@@ -33,13 +47,27 @@ namespace StreetSmartArcGISPro.Logging
         return null;
       }
     }
+    static EventLog()
+    {
+      _logData = LogData.Instance;
+      logCount = _logData.LogCount;
+      lastReset = _logData.LastResetTime;
+    }
 
     public static void Write(EventLogLevel type, string entry, bool flush = false, [CallerMemberName] string methodName = "")
     {
       ArcGIS.Desktop.Framework.Utilities.EventLog.Write(MapEventLogTypeToEventLog(type), entry, flush);
 
-      if(!SentrySdk.IsEnabled)
+      if (!SentrySdk.IsEnabled)
       {
+        return;
+      }
+      ResetCounterIfNeeded();
+
+      if (logCount >= LogsLimit)
+      {
+        ArcGIS.Desktop.Framework.Utilities.EventLog.Write(EventType.Warning, $"Log rate limit reached for the {timeUnit}", true);
+        SentrySdk.CaptureMessage($"Log rate limit exceeded for this {timeUnit}.", SentryLevel.Warning);
         return;
       }
 
@@ -47,9 +75,32 @@ namespace StreetSmartArcGISPro.Logging
       if (loggingLevel == SentryLevel.Error)
       {
         SentrySdk.CaptureMessage(entry, SentryLevel.Error);
+        logCount++;
+        _logData.LogCount = logCount;
+        _logData.LastResetTime = lastReset;
+        _logData.SaveLogData();
         return;
       }
     }
+    private static void ResetCounterIfNeeded()
+    {
+      if (TimeElapsedSinceLastRestart(timeUnit) >= 1)
+      {
+        logCount = 0;
+        lastReset = DateTime.Now;
+        _logData.LogCount = logCount;
+        _logData.LastResetTime = lastReset;
+        _logData.SaveLogData();
+      }
+    }
+
+    private static double TimeElapsedSinceLastRestart(TimeUnit timeUnit) => timeUnit switch
+    {
+      TimeUnit.Minute => (DateTime.Now - lastReset).TotalMinutes,
+      TimeUnit.Hour => (DateTime.Now - lastReset).TotalHours,
+      TimeUnit.Day => (DateTime.Now - lastReset).TotalDays,
+      _ => throw new ArgumentException($"Value {timeUnit.GetType()} not expected", nameof(timeUnit))
+    };
 
     private static SentryLevel MapEventLogTypeToSentryLevel(EventLogLevel type) => type switch
     {
---
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Logging/LogData.cs
@@ -0,0 +1,115 @@
+﻿using Nancy.Xml;
+using StreetSmartArcGISPro.Logging;
+using StreetSmartArcGISPro.Utilities;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+[XmlRoot("LogData")]
+public class LogData
+{
+  [XmlElement("LogCount")]
+  public int LogCount { get; set; }
+
+  [XmlElement("LastResetTime")]
+  public DateTime LastResetTime { get; set; }
+
+  [XmlElement("UseSentryLogging")]
+  public bool UseSentryLogging { get; set; }
+
+  [XmlElement("LogLimit")]
+  public int LogLimit { get; set; }
+
+  [XmlElement("TimeUnit")]
+  public TimeUnit TimeUnit { get; set; }
+
+  public string SentryDsnUrl
+  {
+    get
+    {
+      return "https://d5f8d577e53cfbb3fee7e32ea08a2a69@o4507893926264832.ingest.de.sentry.io/4507893930786896"; // TODO: change to proper production key
+    }
+  }
+
+  private static string LogDataFileName => Path.Combine(FileUtils.FileDir, "logdata.xml");
+  private static LogData _logData;
+
+  public LogData() { }
+
+  public LogData(int logCount, DateTime lastResetTime)
+  {
+    LogCount = logCount;
+    LastResetTime = lastResetTime;
+  }
+  public LogData(int logCount, DateTime lastResetTime, bool useSentryLogging, int logLimit, TimeUnit timeUnit)
+  {
+    LogCount = logCount;
+    LastResetTime = lastResetTime;
+    UseSentryLogging = useSentryLogging;
+    LogLimit = logLimit;
+    TimeUnit = timeUnit;
+  }
+
+  public static LogData Instance
+  {
+    get
+    {
+      if (_logData == null)
+      {
+        LoadLogData();
+      }
+
+      return _logData ?? (_logData = Create());
+    }
+  }
+
+  public static void LoadLogData()
+  {
+    try
+    {
+      if (File.Exists(LogDataFileName))
+      {
+        using (FileStream streamFile = new FileStream(LogDataFileName, FileMode.Open))
+        {
+          XmlSerializer serializer = new XmlSerializer(typeof(LogData));
+          _logData = (LogData)serializer.Deserialize(streamFile);
+          streamFile.Close();
+        }
+      }
+    }
+    catch
+    {
+      EventLog.Write(EventLogLevel.Error, $"Street Smart: (EventLog.cs) (LoadLogData)");
+    }
+  }
+
+  public void SaveLogData()
+  {
+    try
+    {
+      FileStream streamFile = new FileStream(LogDataFileName, FileMode.Create);
+      XmlSerializer serializer = new XmlSerializer(typeof(LogData));
+      serializer.Serialize(streamFile, this);
+      streamFile.Close();
+    }
+
+    catch
+    {
+      EventLog.Write(EventLogLevel.Error, $"Street Smart: (EventLog.cs) (SaveLogData)");
+    }
+  }
+
+  private static LogData Create()
+  {
+    var result = new LogData
+    {
+      LogCount = 0,
+      LastResetTime = DateTime.Now,
+      UseSentryLogging = true,
+      LogLimit = 20,
+      TimeUnit = TimeUnit.Day
+    };
+    result.SaveLogData();
+    return result;
+  }
+}
