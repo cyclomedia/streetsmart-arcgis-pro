23786fd1116f8cf796a3c2c769d563269017531d
Martin Sven Kovačić <mskovacic@cyclomedia.com>
2024-11-07 17:23:40 +0100

    implemented

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Configuration/File/Agreement.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Configuration/File/Agreement.cs
@@ -1,26 +1,25 @@
 ﻿/*
- * Street Smart integration in ArcGIS Pro
- * Copyright (c) 2018 - 2019, CycloMedia, All rights reserved.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
- */
-
-using System.IO;
-using System.Xml.Serialization;
+* Street Smart integration in ArcGIS Pro
+* Copyright (c) 2018 - 2019, CycloMedia, All rights reserved.
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3.0 of the License, or (at your option) any later version.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.
+*/
 
 using StreetSmartArcGISPro.Utilities;
-
+using System;
+using System.IO;
+using System.Xml.Serialization;
 using SystemIOFile = System.IO.File;
 
 namespace StreetSmartArcGISPro.Configuration.File
@@ -46,21 +45,40 @@ namespace StreetSmartArcGISPro.Configuration.File
 
     #region Properties
 
+    public DateTime? AcceptedDate { get; set; }
+
+    public int? AcceptedAgreementVersion { get; set; }
+
+    [XmlIgnore]
+    public readonly DateTime LatestAgreementVersionDate = new DateTime(2024, 11, 7, 16, 41, 00, DateTimeKind.Utc);
+
+    [XmlIgnore]
+    public const int LatestAgreementVersion = 3;
+
+    [XmlIgnore]
     /// <summary>
     /// Value
     /// </summary>
-    public bool Value { get; set; }
+    public bool Value
+    {
+      get => AcceptedDate != null && AcceptedDate >= LatestAgreementVersionDate && AcceptedAgreementVersion != null && AcceptedAgreementVersion >= LatestAgreementVersion;
+      set
+      {
+        if (value)
+        {
+          AcceptedDate = DateTime.Now;
+          AcceptedAgreementVersion = LatestAgreementVersion;
+        }
+      }
+    }
 
     public static Agreement Instance
     {
       get
       {
-        if (_agreement == null)
-        {
-          Load();
-        }
+        _agreement ??= Load();
 
-        return _agreement ?? (_agreement = Create());
+        return _agreement ??= Create();
       }
     }
 
@@ -77,19 +95,31 @@ namespace StreetSmartArcGISPro.Configuration.File
       streamFile.Close();
     }
 
-    private static void Load()
+    private static Agreement Load()
     {
       if (SystemIOFile.Exists(FileName))
       {
-        var streamFile = new FileStream(FileName, FileMode.OpenOrCreate);
-        _agreement = (Agreement) XmlAgreement.Deserialize(streamFile);
-        streamFile.Close();
+        using var streamFile = new FileStream(FileName, FileMode.Open);
+        try
+        {
+          return (Agreement)XmlAgreement.Deserialize(streamFile);
+        }
+        catch
+        {
+          return null;
+        }
+        finally
+        {
+          streamFile.Close();
+        }
       }
+
+      return null;
     }
 
     private static Agreement Create()
     {
-      var result = new Agreement {Value = false};
+      var result = new Agreement { Value = false };
       result.Save();
       return result;
     }
