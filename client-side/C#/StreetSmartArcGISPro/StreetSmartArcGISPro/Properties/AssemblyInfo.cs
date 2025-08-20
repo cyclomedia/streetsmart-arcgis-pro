90a925c6f6dcf57752d5237ef0cfb06f2d220901
Harm Bruinsma <hbruinsma@cyclomedia.com>
2025-08-20 14:26:53 +0200

    ESRI-306: - updated version - latest version .net api (25.5) - build in .net 6

--- client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Properties/AssemblyInfo.cs
+++ client-side/C#/StreetSmartArcGISPro/StreetSmartArcGISPro/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CycloMedia")]
 [assembly: AssemblyProduct("Street Smart for ArcGIS Pro")]
-[assembly: AssemblyCopyright("Copyright © CycloMedia 2024")]
+[assembly: AssemblyCopyright("Copyright © CycloMedia 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("914cc234-6eac-401d-a7a8-96baa1782909")]
 
-[assembly: AssemblyVersion("3.0.25.1")]
-[assembly: AssemblyFileVersion("3.0.25.1")]
+[assembly: AssemblyVersion("3.0.25.3")]
+[assembly: AssemblyFileVersion("3.0.25.3")]
